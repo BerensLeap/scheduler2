761105ac93ae825de775ee9168708bcda17e8ee5
ksmamks <minasithil33@gmail.com>
2025-02-13 19:30:05 +0900

    errorFix

--- src/main/java/com/example/scheduleApp/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/scheduleApp/common/exception/GlobalExceptionHandler.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     // 여러 예외를 한 번에 처리
-    @ExceptionHandler({AuthenticationFailException.class, NoSuchEmailException.class})
+    @ExceptionHandler(AuthenticationFailException.class)
     public ResponseEntity<String> handleAllException(RuntimeException ex, HttpServletRequest request) {// 예외 메시지와 함께 HTTP 상태 코드 반환
         String message = ex.getMessage();
         return new ResponseEntity<>(message, HttpStatus.UNAUTHORIZED);  // HTTP Status 401 반환
